fa605982a8d23838240d766943e61da1277527f5
Yaroslav-Zozulya <my.dev.stories@gmail.com>
2023-11-28 17:10:20 +0300

    done

--- front/src/app/page.js
+++ front/src/app/page.js
@@ -2,13 +2,14 @@ import Contact from "@/components/pages/home/Contact/Contact";
 import Courses from "@/components/pages/home/Courses/Courses.module";
 import Hero from "@/components/pages/home/hero";
 import StepSection from "@/components/pages/home/Step";
-
+import Faq from "@/components/pages/home/Faq/Faq";
 export default function Home() {
   return (
     <>
       <Hero />
       <StepSection />
       <Courses />
+      <Faq />
       <Contact />
     </>
   );
