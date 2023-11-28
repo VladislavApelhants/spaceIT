ea1f714ab54768b8cd9d2e9f5ec517eab3b23a79
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2023-11-28 16:15:36 +0200

    not adaptive image

--- front/src/app/page.js
+++ front/src/app/page.js
@@ -1,6 +1,7 @@
 import Contact from "@/components/pages/home/Contact/Contact";
 import Courses from "@/components/pages/home/Courses/Courses.module";
 import Hero from "@/components/pages/home/hero";
+import HowLearning from "@/components/pages/home/HowLearning/HowLearning";
 import StepSection from "@/components/pages/home/Step";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <StepSection />
       <Courses />
+      <HowLearning />
       <Contact />
     </>
   );
