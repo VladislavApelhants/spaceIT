84c6090ccd2db3fa78191f1499c2210aaa24410d
Yaroslav-Zozulya <my.dev.stories@gmail.com>
2023-11-29 17:51:10 +0300

    done

--- front/src/app/page.js
+++ front/src/app/page.js
@@ -3,6 +3,7 @@ import Courses from "@/components/common/Courses/Courses.module";
 import Hero from "@/components/pages/home/hero";
 import HowLearning from "@/components/pages/home/HowLearning/HowLearning";
 import StepSection from "@/components/pages/home/Step";
+import JoinUs from "@/components/pages/home/JoinUs/JoinUs";
 import Faq from "@/components/pages/home/Faq/Faq";
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <StepSection />
       <Courses />
       <HowLearning />
+      <JoinUs />
       <Faq />
       <Contact />
     </>
