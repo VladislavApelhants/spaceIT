e6036ae26cea9b4d4110d0b0ecba9d318ae6a866
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2023-11-29 21:14:39 +0200

    change name class

--- front/src/components/common/Courses/LinkCourse/LinkCourse.jsx
+++ front/src/components/common/Courses/LinkCourse/LinkCourse.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
-import s from "./button.module.scss";
-import Image from "next/image";
+import s from "./LinkCourse.module.scss";
+
 export default function LinkCourse() {
   return (
     <Link href="/course" className={s.button}>
