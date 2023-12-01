2cde3f3e2060e926c39d87d2d9d0eac59a5e70ec
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2023-12-01 21:46:44 +0200

    com

--- front/src/components/pages/course/Hero/Hero.jsx
+++ front/src/components/pages/course/Hero/Hero.jsx
@@ -1,11 +1,30 @@
 import Container from "@/components/common/Container/Container";
-import s from "./Hero.module.scss";
+import Title from "@/components/common/Title/Title";
+import s from "./hero.module.scss";
+import Button from "@/components/common/Button/Button";
+import HeroTechnologies from "./Hero_technologies/HeroTechnologies";
 export default function Hero() {
   return (
-    <section>
+    <section className={s.hero_section}>
+      <Title
+        text="Our Courses"
+        className={"main_courses_title"}
+        tagName={"h1"}
+      />
       <Container>
         <div className={s.hero_vrapper}>
-          <Title />
+          <div className={s.title_box}>
+            <Title
+              text="UI/UX DESIGN"
+              className={"courses_hero_title"}
+              tagName={"h2"}
+            />
+            <p className={s.hero_description}>
+              Навчіться професійно розроблювати дизайн сайтів та застосунків
+            </p>
+            <Button text="ЗАПИСАТИСЬ НА КУРС" className="hero_course_button" />
+          </div>
+          <HeroTechnologies />
         </div>
       </Container>
     </section>
