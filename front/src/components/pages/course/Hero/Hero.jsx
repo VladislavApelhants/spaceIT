2b06688ad514c504937a93a265fa606e4da2bf9b
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2023-12-01 15:12:55 +0200

    q

--- front/src/components/pages/course/Hero/Hero.jsx
+++ front/src/components/pages/course/Hero/Hero.jsx
@@ -1,4 +1,13 @@
+import Container from "@/components/common/Container/Container";
 import s from "./Hero.module.scss";
 export default function Hero() {
-  return <section></section>;
+  return (
+    <section>
+      <Container>
+        <div className={s.hero_vrapper}>
+          <Title />
+        </div>
+      </Container>
+    </section>
+  );
 }
