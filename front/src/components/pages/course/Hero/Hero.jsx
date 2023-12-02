f6ba6cf72533273a6c7d683147d2b7e3669f4d9e
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2023-12-02 23:21:57 +0200

    kon-chen-in

--- front/src/components/pages/course/Hero/Hero.jsx
+++ front/src/components/pages/course/Hero/Hero.jsx
@@ -3,6 +3,8 @@ import Title from "@/components/common/Title/Title";
 import s from "./hero.module.scss";
 import Button from "@/components/common/Button/Button";
 import HeroTechnologies from "./Hero_technologies/HeroTechnologies";
+import HeroPresentation from "./HeroPresentation/HeroPresentation";
+
 export default function Hero() {
   return (
     <section className={s.hero_section}>
@@ -12,19 +14,25 @@ export default function Hero() {
         tagName={"h1"}
       />
       <Container>
-        <div className={s.hero_vrapper}>
-          <div className={s.title_box}>
-            <Title
-              text="UI/UX DESIGN"
-              className={"courses_hero_title"}
-              tagName={"h2"}
-            />
-            <p className={s.hero_description}>
-              Навчіться професійно розроблювати дизайн сайтів та застосунків
-            </p>
-            <Button text="ЗАПИСАТИСЬ НА КУРС" className="hero_course_button" />
+        <div className={s.hero_all_vrapper}>
+          <HeroPresentation />
+          <div className={s.hero_info_vrapper}>
+            <div className={s.title_box}>
+              <Title
+                text="UI/UX DESIGN"
+                className={"courses_hero_title"}
+                tagName={"h2"}
+              />
+              <p className={s.hero_description}>
+                Навчіться професійно розроблювати дизайн сайтів та застосунків
+              </p>
+              <Button
+                text="ЗАПИСАТИСЬ НА КУРС"
+                className="hero_course_button"
+              />
+            </div>
+            <HeroTechnologies />
           </div>
-          <HeroTechnologies />
         </div>
       </Container>
     </section>
