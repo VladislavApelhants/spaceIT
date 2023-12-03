d5b0a6a56b93860ae31b6aadd900eac4a9e1e683
Yaroslav-Zozulya <my.dev.stories@gmail.com>
2023-12-04 01:08:26 +0300

    done

--- front/src/app/course/page.js
+++ front/src/app/course/page.js
@@ -1,10 +1,12 @@
 import Courses from "@/components/common/Courses/Courses.module";
 import Hero from "@/components/pages/course/Hero/Hero";
+import AboutCourse from "@/components/pages/course/Hero/AbooutCourse/AbooutCourse";
 
 export default function Course() {
   return (
     <>
       <Hero />
+      <AboutCourse />
       <Courses />
     </>
   );
--- front/src/components/common/CourseContainer/CourseContainer.jsx
+++ front/src/components/common/CourseContainer/CourseContainer.jsx
@@ -1,4 +1,4 @@
-import s from "./container.module.scss";
+import s from "./courseContainer.module.scss";
 export default function CourseContainer({ children }) {
   return <div className={s.courseContainer}>{children}</div>;
 }
