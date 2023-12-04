2e48e6ad37724ba65afab9452764329285c7d2e4
Yaroslav-Zozulya <my.dev.stories@gmail.com>
2023-12-04 22:29:13 +0300

    done

--- front/src/app/course/page.js
+++ front/src/app/course/page.js
@@ -1,8 +1,9 @@
 import Hero from "@/components/pages/course/Hero/Hero";
-import AboutCourse from "@/components/pages/course/Hero/AbooutCourse/AbooutCourse";
+import AboutCourse from "@/components/pages/course/AbooutCourse/AbooutCourse";
 import YouLearning from "@/components/pages/course/YouLearning/YouLearning";
 import Courses from "@/components/pages/course/Courses/Courses.module";
 import Contact from "@/components/pages/home/Contact/Contact";
+import AboutProfession from "@/components/pages/course/AboutProfession/AboutProfession";
 
 export default function Course() {
   return (
@@ -10,6 +11,7 @@ export default function Course() {
       <Hero />
       <AboutCourse />
       <YouLearning />
+      <AboutProfession />
       <Courses />
       <Contact />
     </>
