c65d30ceb4199a09dcb5089afeaf4b38f4d1385e
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2023-12-05 14:44:56 +0200

    done

--- front/src/app/course/page.js
+++ front/src/app/course/page.js
@@ -5,7 +5,7 @@ import Courses from "@/components/pages/course/Courses/Courses.module";
 import Contact from "@/components/pages/home/Contact/Contact";
 import AboutProfession from "@/components/pages/course/AboutProfession/AboutProfession";
 import Teachers from "@/components/pages/course/Teachers/Teachers";
-
+import Ready from "@/components/pages/course/Ready/Ready";
 
 export default function Course() {
   return (
@@ -15,6 +15,7 @@ export default function Course() {
       <YouLearning />
       <AboutProfession />
       <Teachers />
+      <Ready />
       <Courses />
       <Contact />
     </>
---
+++ front/src/components/pages/course/Ready/Forms/Form.jsx
@@ -0,0 +1,55 @@
+"use client";
+
+import axios from "axios";
+import s from "./styles.module.scss";
+
+export function Form({
+  btnText,
+  endpoint,
+  subtext = "Вкажіть який текст",
+  subtextStatus = true,
+}) {
+  const handlerSubmit = async (e) => {
+    e.preventDefault();
+    const data = {};
+    new FormData(e.currentTarget).forEach((value, key) => {
+      data[key] = value;
+    });
+    console.log("set endpoint for request ", data);
+    // await axios.post(endpoint, data);
+  };
+
+  return (
+    <form className={s.form} onSubmit={handlerSubmit}>
+      <div className={s.btn_wrapper}>
+        <div className={s.inputWrapper}>
+          <input
+            type="text"
+            className={s.input}
+            placeholder="ПІБ"
+            name="name"
+            required
+          />
+          <input
+            type="email"
+            className={s.input}
+            placeholder="Email"
+            name="email"
+            required
+          />
+          <input
+            type="tel"
+            className={s.input}
+            placeholder="Номер телефону"
+            name="phone"
+            required
+          />
+        </div>
+        <button type="submit" className={s.btn}>
+          {btnText}
+        </button>
+      </div>
+      {subtextStatus && <p className={s.text}>{subtext}</p>}
+    </form>
+  );
+}
