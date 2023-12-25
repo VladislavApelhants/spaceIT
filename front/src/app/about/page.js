3df483f9531ae02643060875f16047e4095f8786
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2023-12-25 22:36:22 +0200

    add base

--- front/src/app/about/page.js
+++ front/src/app/about/page.js
@@ -2,11 +2,22 @@ import LectureHall from "@/components/pages/about/LectureHall/LectureHall";
 import Contact from "@/components/pages/home/Contact/Contact";
 import HiddenWrapper from "@/components/common/HiddenWrapper/HiddenWrapper";
 import OurTeam from "@/components/pages/about/OurTeam/OurTeam";
+import Hero from "@/components/pages/about/Hero/Hero";
+import Image from "next/image";
+import s from "./styles.module.scss";
 
 export default function About() {
   return (
     <>
+      <Image
+        src="/icons/unionAboutUs.svg"
+        width={4041}
+        height={4031}
+        alt="декоративне зображення коло"
+        className={s.hero_union}
+      />
       <HiddenWrapper>
+        <Hero />
         <LectureHall />
         <OurTeam />
         <Contact />
