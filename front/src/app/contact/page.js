c9e22997043c3ae532d7446b0317673709ff9473
Yaroslav-Zozulya <my.dev.stories@gmail.com>
2023-12-30 16:15:56 +0300

    done

--- front/src/app/contact/page.js
+++ front/src/app/contact/page.js
@@ -2,12 +2,15 @@ import Contact from "@/components/pages/home/Contact/Contact";
 import TopDecor from "@/components/common/TopDecor/TopDecor";
 import HiddenWrapper from "@/components/common/HiddenWrapper/HiddenWrapper";
 import Contacts from "@/components/pages/contact/Contacts/Contacts";
+import Map from "@/components/pages/contact/Contacts/Map/Map";
+
 export default function ContactUs() {
   return (
     <>
       <TopDecor />
       <HiddenWrapper>
         <Contacts />
+        <Map />
         <Contact />
       </HiddenWrapper>
     </>
