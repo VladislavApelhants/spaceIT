5b1d8e491c4a8cd2ef466153d46851881993c341
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2023-12-30 16:38:12 +0200

    add fix

--- front/src/app/tech/page.js
+++ front/src/app/tech/page.js
@@ -1,4 +1,18 @@
+import TechSection from "@/components/pages/tech/Tech";
 import s from "./page.module.scss";
+import Image from "next/image";
 export default function Tech() {
-  return <h1 className={s.color}>Tech</h1>;
+  return (
+    <>
+      {/* <Image
+        src="/images/tech.png"
+        width={2179}
+        height={2240}
+        alt="фонове зображення з планетами"
+        className={s.image}
+      /> */}
+      {/* <div className={s.image}></div> */}
+      <TechSection />
+    </>
+  );
 }
