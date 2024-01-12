48bc0707b3ab0eac55ccb9f4a5cad1ad82158a04
Yaroslav-Zozulya <my.dev.stories@gmail.com>
2024-01-12 16:32:43 +0300

    add send email and fix bug with modal

--- front/src/components/pages/contact/Contacts/Contacts.jsx
+++ front/src/components/pages/contact/Contacts/Contacts.jsx
@@ -2,8 +2,8 @@ import Title from "@/components/common/Title/Title";
 import s from "./contacts.module.scss";
 import { Form } from "@/components/common/Forms/Form";
 import Container from "@/components/common/Container/Container";
-
 import ContactsBox from "./ContactsBox/ContactsBox";
+import { endpoints } from "@/services/serverAPI";
 
 export default function Contacts() {
   return (
@@ -14,7 +14,10 @@ export default function Contacts() {
           <ContactsBox />
           <div className={s.form_wrapper}>
             <p className={s.form_description}>БЕЗКОШТОВНА КОНСУЛЬТАЦІЯ</p>
-            <Form btnText="ЗАМОВИТИ КОНСУЛЬТАЦІЮ" />
+            <Form
+              btnText="ЗАМОВИТИ КОНСУЛЬТАЦІЮ"
+              endpoint={endpoints.callback}
+            />
           </div>
         </div>
       </Container>
