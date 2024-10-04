a57c23225fe313eacf7c9a6c506e6564f55ef616
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2024-10-04 16:48:23 +0300

    q

--- front/src/services/serverAPI.js
+++ front/src/services/serverAPI.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 // const BASE_URL = "https://roomy-tray-production.up.railway.app";
-const BASE_URL = "https://gentle-cooperation-production.up.railway.app/";
+const BASE_URL = "https://gentle-cooperation-production.up.railway.app";
 
 export const endpoints = {
   callbackCourse: `${BASE_URL}/api/callback-courses`,
   callback: `${BASE_URL}/api/callbacks`,
   getAllCourses: `${BASE_URL}/api/courses?populate=previewImg`,
-  BASE_URL: "https://gentle-cooperation-production.up.railway.app/",
+  BASE_URL: "https://gentle-cooperation-production.up.railway.app",
 };
 
 async function sendModalCall(endpoint, data) {
