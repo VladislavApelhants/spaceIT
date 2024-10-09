efdd438e10954d06fb430eca51634dfc03514618
Vladislav <93011297+VladislavApelhants@users.noreply.github.com>
2024-10-09 17:24:14 +0300

    fix static files

--- server/config/middlewares.js
+++ server/config/middlewares.js
@@ -1,12 +1,18 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      origin: ["https://spaceit-production.up.railway.app"], // Укажи домены, с которых разрешены запросы
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
